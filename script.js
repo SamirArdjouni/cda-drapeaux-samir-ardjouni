d77662aa53484fc03c4524d8c7da284f7ebaa490
samir ardjouni <samir.ardjouni@gmail.com>
2021-01-23 12:48:52 +0100

    Q2 verif si couleur flag est OK ou KO

--- script.js
+++ script.js
@@ -1,93 +1,40 @@
 var couleur = [ 'white', 'red','blue', 'yellow', 'green', 'black'];
 var france = "blue/white/red";
-var i = 0;
+let map = new Map(); // creation tableau map a 2valeurs
+map.set('france', 'blue/white/red');
+
+// recuperer on fait map.('paris');
+var i=0,j=0,k = 0;
 
 // ***** FONCTION CHANGEMENT COULEURS ***************
 
 $( "#flagLeft" ).click(function() {
     i == couleur.length ? i=0 : i++;
-   
     var colore = couleur[i];
     $(this).css("backgroundColor", colore);
   });
 
   $( "#flagMiddle" ).click(function() {
-    i == couleur.length ? i=0 : i++;
-   
-    var colore = couleur[i];
+    j == couleur.length ? j=0 : j++;
+    var colore = couleur[j];
     $(this).css("backgroundColor", colore);
   });
 
   $( "#flagRight" ).click(function() {
-    i == couleur.length ? i=0 : i++;
-   
-    var colore = couleur[i];
+    k == couleur.length ? k=0 : k++;
+    var colore = couleur[k];
     $(this).css("backgroundColor", colore);
+    
   });
 
-//   ************************************************
+//   *************** bouton valider *********************************
 
-
-
-
-// (function(){
-//     var couleur =  document.querySelector("#flagLeft");
-//     couleur.addEventListener('click', function(){
-
-//         couleur.style.backgroundColor = 'blue'; 
-//         couleur.textContent='tentative de modification';  
-//         couleur.innerHTML='<strong>Texte en gras</strong>';
-
-//     });
-// });
-   
-
-
-// $(document).ready(function(){
-//     $("#flagLeft").click(function(){
-//         var p = "#flagLeft";
-//         p.css()("background-color", "blue");
-        
-//     })
-// })
-
-// (function(){
-//    var couleur =  document.getElementById("#flagLeft");
-//    couleur.click(function(){
-//        couleur.style.backgroundColor = 'blue';      
-
-//    })
-   
-// });
-
-
-// $(document).ready(function () {
-//     $("#buttonValidate").click(function () {
-//         $("#contener").hide();
-//         alert("bonjour");
-//     });
-
-// });
-
-
-
-
-// function init() {
-//     $("#buttonValidate").click(function(){
-//         alert("hello");
-
-//     })
-//  }
-
-// window.onload = init;
-
-
-// (function(){
-//     var couleur =  document.getElementById("#flagLeft");
-//     couleur.addEventListener('click', fo-nctionChangeCouleur);
-//     function nctionChangeCouleur (){
-//         couleur.style.backgroundColor = 'blue';      
- 
-//     }
-
-// })
+$("#buttonValidate").click(function(){
+    var country = couleur[i] + '/' + couleur[j] + '/' +couleur[k];
+    var p = document.getElementById("buttonValidate");
+    if (map.get("france") == country ) {
+        console.log("bingo c'est trouvé");
+    }else{
+        console.log("c'est raté"); 
+    }
+})
